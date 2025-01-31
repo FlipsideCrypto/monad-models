3796ab63eb6e3bb06fe8ed23d3b17bfa575552fe
Austin <austin@flipsidecrypto.com>
2025-01-31 14:46:18 -0500

    updates

---
+++ models/github_actions/github_actions__current_task_status.sql
@@ -0,0 +1,9 @@
+{# Log configuration details #}
+{{ log_model_details() }}
+
+{{ config(
+    materialized = 'view',
+    tags = ['gha_tasks']
+) }}
+
+{{ fsc_utils.gha_task_current_status_view() }}
