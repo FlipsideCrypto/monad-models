f4182375b148b87722da4113f0f1bb766af3f879
drethereum <trevor.wenokur@gmail.com>
2025-04-18 14:27:33 -0600

    exclude

--- models/testnet/core/gold/testnet__fact_event_logs.sql
+++ models/testnet/core/gold/testnet__fact_event_logs.sql
@@ -18,7 +18,7 @@ WITH base AS (
     WHERE
         1 = 1
         AND ARRAY_SIZE(receipts_json :logs) > 0
-
+        AND ROUND(block_number,-3) != 12354000
 {% if is_incremental() %}
 AND modified_timestamp > (
     SELECT
